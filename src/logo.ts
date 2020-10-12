72aaf0a9e26cf8159ac8380f2d900f1c9fbaca04
Nathan Chappell <nathan.s.chappell@gmail.com>
2020-10-12 07:32:21 +0200

    lighter

--- src/logo.ts
+++ src/logo.ts
@@ -1,110 +1,247 @@
 import { v4 } from "uuid";
 
-import { C, CSVG, parseSVG, makeRingId } from "./util";
-import { attachRingProxies } from "./logoAnimationProxy";
-
-type Radius = number;
-type BorderWidth = number;
 type Degrees = number;
-type Level = number;
 
 const deg2rad = (d: Degrees) => (Math.PI * d) / 180;
 
-/*
-<animateTransform 
-  attributeName="transform"
-  type="rotate"
-  by="360"
-  dur="3s"
-  repeatCount="indefinite"
-  fill="freeze"
-  additive="sum"
-  />
-*/
+const defaultViewBox = "-1 -1 2 2";
 
 interface RingProps {
-  arc: Degrees;
-  phase: Degrees;
-  //r: Radius;
-  l: Level;
+  radius: number;
+  arcLength: Degrees;
+  strokeWidth: number;
+  color: string;
+  viewBox: string;
 }
 
 const defaultRingProps: RingProps = {
-  arc: 90,
-  phase: 0,
-  //r: 0.25,
-  l: 1,
+  radius: 0.2,
+  arcLength: 270,
+  strokeWidth: 0.1,
+  color: "hsl(250,80%,50%,.8)",
+  viewBox: defaultViewBox,
 };
 
-const r = 0.25;
-
-const RingA = (rr: number, x1: number, y1: number, x2: number, y2: number) =>
-  `M ${x1} ${y1} A ${rr} ${rr} 0 0 0 ${x2} ${y2} `;
-
-const Ring = (id: string, props: Partial<RingProps>) => {
-  console.log("making ring:", id);
-  const { arc, phase, /*r,*/ l } = { ...defaultRingProps, ...props };
-  const _arc = arc > 360 ? arc % 360 : arc;
-  const rr = r * l;
-  const x1 = rr;
-  const y1 = 0;
-  const x2 = rr * Math.cos(deg2rad(_arc / 2));
-  const y2 = rr * Math.sin(-deg2rad(_arc / 2));
-  const x3 = rr * Math.cos(deg2rad(_arc));
-  const y3 = rr * Math.sin(-deg2rad(_arc));
-  const A1 = RingA(rr, x1, y1, x2, y2);
-  const A2 = RingA(rr, x2, y2, x3, y3);
-  const path = parseSVG(`
-    <path
-      id="${id}"
-      d="${A1} ${A2}"
-      transform="rotate(${-phase})"
-      stroke="hsl(120,50%,50%,.75)"
-      stroke-width="${r / 2}"
-      fill="none"
-      stroke-linecap="round"
-    />`);
-  return path;
-};
-
-interface LogoSpec {
-  rings: Partial<RingProps>[];
-  id?: string;
+interface RingSVGResult {
+  svg: string;
+  motionID: string;
+  strokeID: string;
+  rotateID: string;
+  innerID: string;
 }
 
-const defaultLogoSpec: LogoSpec = {
-  rings: [
-    { l: 1, arc: 90, phase: 0 },
-    { l: 2, arc: 180, phase: 0 },
-    { l: 3, arc: 270, phase: 0 },
-    { l: 4, arc: 360 },
-  ],
-  id: "",
+const RingSVG: (props: RingProps) => RingSVGResult = ({
+  radius,
+  arcLength,
+  strokeWidth,
+  color,
+  viewBox,
+}) => {
+  const motionID = v4();
+  const strokeID = v4();
+  const rotateID = v4();
+  const innerID = v4();
+  const x1 = radius;
+  const y1 = 0;
+  const x2 = radius * Math.cos(deg2rad(arcLength));
+  const y2 = radius * Math.sin(-deg2rad(arcLength));
+  const longArc = arcLength > 180 ? 1 : 0;
+  const d = `M ${x1} ${y1} 
+             A ${radius} ${radius}
+               0 ${longArc} 0
+               ${x2} ${y2}`;
+  const commonAttrs = `dur="1s" calcMode="spline" keySplines="0 .5 .5 0" repeatCount="1"`;
+  const svg = `
+    <g>
+      <animateMotion 
+        id="${motionID}"
+        path="M 0 0" 
+        ${commonAttrs} 
+      />
+      <svg 
+        id="${innerID}"
+        viewBox="${viewBox}"
+        x="-1"
+        y="-1"
+      >
+        <path
+          d="${d}"
+          stroke="${color}"
+          stroke-width="${strokeWidth}"
+          fill="none"
+          stroke-linecap="round"
+        >
+          <animate 
+            id="${strokeID}"
+            attributeName="stroke" 
+            values="${color};${color}" 
+            ${commonAttrs} 
+          />
+          <animateTransform
+            id="${rotateID}"
+            attributeName="transform"
+            type="rotate"
+            values="0;0"
+            ${commonAttrs}
+          />
+        </path>
+      </svg>
+    </g>
+  `;
+  return { svg, motionID, strokeID, rotateID, innerID };
 };
 
-export const Logo = ({ id, rings }: LogoSpec = defaultLogoSpec) => {
-  let _id: string;
-  if (id === undefined || id === "") {
-    _id = v4();
+const getElementOrThrow = (id: string) => {
+  const el = document.getElementById(id);
+  if (el === null) {
+    throw Error(`null element. id: ${id}`);
   } else {
-    _id = id;
+    return el;
   }
-  const viewBox = "-1.125 -1.125 2.25 2.25";
-  let svg = CSVG();
-  svg.setAttribute("viewBox", viewBox);
-  const ringElements = rings.map((ringProps, i) =>
-    Ring(makeRingId(_id, i), ringProps)
-  );
-  ringElements.forEach((ring) => svg.appendChild(ring));
-  const e = document.createElement("div");
-  e.id = _id;
-  e.className = "logo-proto";
-  e.appendChild(svg);
-  // fucked up stupid hack...
-  e.innerHTML = e.innerHTML;
-  svg = e.children[0] as SVGSVGElement;
-  //const proxies = attachRingProxies(e, "linear", "circular");
-  const proxies = attachRingProxies(e, "linear","sequential");
-  (document as any).proxies = proxies;
-  return e;
+};
+
+interface AnimationSpec {
+  val: string;
+  dur?: string;
+  keySplines?: string;
+  repeatCount?: string;
+}
+
+const transition = (spec: AnimationSpec, el: SVGAnimationElement) => {
+  // if you put multiple vals, I assume you know what you're doing
+  if (spec.val.match(/;/)) {
+    el.setAttribute("values", spec.val);
+  } else {
+    const oldVals = el.getAttribute("values")?.split(/;/);
+    const from = oldVals ? oldVals[oldVals.length - 1] : undefined;
+    if (from === 'undefined') {
+      throw Error(`couldn't get from val: ${spec}, ${el}`);
+    } else {
+      el.setAttribute("values", `${from};${spec.val}`);
+    }
+  }
+  Object.entries(spec)
+    .filter(([k, v]) => k !== "val")
+    .forEach(([k, v]) => el.setAttribute(k, v));
+  el.setAttribute("begin", el.getCurrentTime().toString());
+  /*
+  if (spec.dur !== undefined) {
+    el.setAttribute('dur',spec.dur);
+  }
+  if (spec.keySplines !== undefined) {
+    el.setAttribute('keySplines',spec.keySplines);
+  }
+  if (spec.repeatCount !== undefined) {
+    el.setAttribute('repeatCount',spec.repeatCount);
+  }
+  */
+};
+
+type BufferMode = "repeat" | "once";
+
+interface RingModes {
+  rotations: BufferMode;
+  colors: BufferMode;
+  motions: BufferMode;
+}
+
+const defaultModes: RingModes = {
+  rotations: "repeat",
+  colors: "once",
+  motions: "once",
+};
+
+export class Ring {
+  ringSVG: RingSVGResult;
+  modes: RingModes;
+  rotations: AnimationSpec[] = [];
+  colors: AnimationSpec[] = [];
+  motions: AnimationSpec[] = [];
+
+  constructor(props: Partial<RingProps>) {
+    this.ringSVG = RingSVG({ ...defaultRingProps, ...props });
+    this.modes = { ...defaultModes };
+  }
+
+  /*
+   * are these getters possible performance issues?
+   */
+  get rotateEl(): SVGAnimationElement {
+    return (getElementOrThrow(
+      this.ringSVG.rotateID
+    ) as unknown) as SVGAnimationElement;
+  }
+
+  get colorEl(): SVGAnimationElement {
+    return (getElementOrThrow(
+      this.ringSVG.strokeID
+    ) as unknown) as SVGAnimationElement;
+  }
+
+  get motionEl(): SVGAnimationElement {
+    return (getElementOrThrow(
+      this.ringSVG.motionID
+    ) as unknown) as SVGAnimationElement;
+  }
+
+  start() {
+    this.next("rotation");
+    this.next("color");
+    this.next("motion");
+  }
+
+  next(attr: "rotation" | "color" | "motion") {
+    let q: AnimationSpec[];
+    let el: SVGAnimationElement;
+    let mode: BufferMode;
+    switch (attr) {
+      case "rotation":
+        q = this.rotations;
+        el = this.rotateEl;
+        mode = this.modes.rotations;
+        break;
+      case "color":
+        q = this.colors;
+        el = this.colorEl;
+        mode = this.modes.colors;
+        break;
+      case "motion":
+        q = this.motions;
+        el = this.motionEl;
+        mode = this.modes.motions;
+        break;
+      default:
+        throw Error(`bad next: ${attr}`);
+    }
+    const nextSpec = q.shift();
+    if (nextSpec === undefined) {
+      return;
+    } else {
+      transition(nextSpec, el);
+      el.addEventListener(
+        "endEvent",
+        () => {
+          this.next(attr);
+        },
+        { once: true }
+      );
+      if (mode === "repeat") {
+        q.push(nextSpec);
+      }
+    }
+  }
+}
+
+export const Logo = () => {
+  const div = document.createElement("div");
+  div.id = "logo-div";
+  div.className = "logo-proto";
+  div.innerHTML = `
+    <svg viewBox="${defaultViewBox}">
+    </svg>
+  `;
+  const svg = div.children[0];
+  const rings = [1,2,3,4].map(x => new Ring({radius: x*.2}))
+  svg.innerHTML = rings.map(ring => ring.ringSVG.svg).join("\n");
+  return div;
 };
